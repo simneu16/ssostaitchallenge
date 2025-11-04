7e39f3c9f11a839b37e33c5d8f16500f00bacdeb
simneu16 <165833687+simneu16@users.noreply.github.com>
2025-11-04 15:12:54 +0100

    Update db_connection.php

--- php/db_connection.php
+++ php/db_connection.php
@@ -2,10 +2,10 @@
 function OpenConnection()
 {
     $host = 'db.r1.websupport.sk';
-    $port = '5432'; // Change if your DB runs on a different port
-    $dbname = 'unitmate'; // Change to your database name
-    $user = 'unitadmin '; // Change to your DB user
-    $password = '.Lo9,ki8'; // Change to your DB password
+    $port = '5432';
+    $dbname = 'unitmate';
+    $user = 'unitadmin';
+    $password = '.Lo9,ki8';
 
     $conn_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
     $dbconn = pg_connect($conn_string);
